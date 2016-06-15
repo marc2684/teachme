80380f2c123d16603c1ddea1a0f04f3d88a5542d
marc2684 <antonio_corona9@hotmail.com>
2016-06-15 12:08:23 -0500

    Creando ciclo en el seeder Users

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,10 +2,21 @@
 
 use Illuminate\Database\Seeder;
 use TeachMe\Entities\User;
+use Faker\Factory as Faker;
 
 class UserTableSeeder extends Seeder {
 
     public function run(){
+        $faker = Faker::create();
+
+        for ($i = 1; $i <= 50; $i++){
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => 'secret'
+            ])
+        }
+
         $this->createAdmin();
     }
 
